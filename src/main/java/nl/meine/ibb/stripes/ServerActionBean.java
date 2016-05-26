02f4e5e80f48bf2b1fd2446f81d329f1ea4abe87
Meine Toonen <toonen.meine@gmail.com>
2016-05-26 21:01:10 +0200

    Initial commit

---
+++ src/main/java/nl/meine/ibb/stripes/ServerActionBean.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.meine.ibb.stripes;
+
+import net.sourceforge.stripes.action.ActionBean;
+import net.sourceforge.stripes.action.ActionBeanContext;
+import net.sourceforge.stripes.action.DefaultHandler;
+import net.sourceforge.stripes.action.ForwardResolution;
+import net.sourceforge.stripes.action.Resolution;
+import net.sourceforge.stripes.action.UrlBinding;
+
+/**
+ *
+ * @author meine
+ */
+@UrlBinding("/action/server")
+public class ServerActionBean implements ActionBean{
+
+    private ActionBeanContext context;
+
+    @Override
+    public ActionBeanContext getContext() {
+        return context;
+    }
+
+    @Override
+    public void setContext(ActionBeanContext context) {
+        this.context = context;
+    }
+  
+    @DefaultHandler
+    public Resolution poll(){
+        return new ForwardResolution("/WEB-INF/newjsp.jsp");
+    }
+}
