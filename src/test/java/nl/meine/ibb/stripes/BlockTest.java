674841cefff5ab5d1ca8d2d357265c556d0719ca
Meine Toonen <toonen.meine@gmail.com>
2016-06-09 22:58:32 +0200

    fixed write function

--- src/test/java/nl/meine/ibb/stripes/BlockTest.java
+++ src/test/java/nl/meine/ibb/stripes/BlockTest.java
@@ -9,59 +9,58 @@ import java.util.Arrays;
 import org.junit.Test;
 
 public class BlockTest {
-	
-	@Test
-	public void test() {
+
+    @Test
+    public void test() {
 //		Block block = new Block(0);
 //		block.addPosition(10, 10);
 //		block.addPosition(12, 12);
 //block.finish();
 //		System.out.println(block);
-		
-		
-		// 4009 4001   [FA9FA1]  
-		int d = Block.hex(4009, 4001);
-		System.out.println(Long.toHexString(d));
-	}
-	
-	
-	@Test
-	public void testStart() {		
-		int val = Block.hex(4009, 4001);
-		System.out.println(val);
-		assertEquals("fa9fa1", Long.toHexString(val));
-	}
-	
-	@Test
-	public void testBlock() {
-		Block block = new Block(0);
-		block.addPosition(0, 100);
-		block.addPosition(100, 0);
-		block.finish();
-		System.out.println(block);
-		//assertEquals("fa9fa1", Long.toHexString(Block.hex(4009, 4001)));
-	}
-	
-	@Test
-	public void testStream() throws IOException {
-		Block block = new Block(1);
-		ByteArrayOutputStream os = new ByteArrayOutputStream();
-		block.write(os);
-		
-		byte[] s = os.toByteArray();
-		System.out.println(Arrays.toString(s));
-		System.out.println();
-		System.out.println(bytesToHex(s));
-	}
-	
-	final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
-	public static String bytesToHex(byte[] bytes) {
-	    char[] hexChars = new char[bytes.length * 2];
-	    for ( int j = 0; j < bytes.length; j++ ) {
-	        int v = bytes[j] & 0xFF;
-	        hexChars[j * 2] = hexArray[v >>> 4];
-	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-	    }
-	    return new String(hexChars);
-	}
+
+        // 4009 4001   [FA9FA1]  
+        int d = Block.hex(4009, 4001);
+        System.out.println(Long.toHexString(d));
+    }
+
+    @Test
+    public void testStart() {
+        int val = Block.hex(4009, 4001);
+        System.out.println(val);
+        assertEquals("fa9fa1", Long.toHexString(val));
+    }
+
+    @Test
+    public void testBlock() {
+        Block block = new Block(0);
+        block.addPosition(0, 100);
+        block.addPosition(100, 0);
+        block.finish();
+        System.out.println(block);
+        //assertEquals("fa9fa1", Long.toHexString(Block.hex(4009, 4001)));
+    }
+
+    @Test
+    public void testStream() throws IOException {
+        Block block = new Block(1);
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        block.write(os);
+        
+        byte[] s = os.toByteArray();
+        System.out.println(Arrays.toString(s));
+        System.out.println();
+        System.out.println(bytesToHex(s));
+    }
+
+    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
 }
