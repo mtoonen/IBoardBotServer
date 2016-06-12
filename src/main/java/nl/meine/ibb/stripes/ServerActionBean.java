e38438763236059a811e366730ca7770ad67a288
Meine Toonen <toonen.meine@gmail.com>
2016-06-12 12:08:29 +0200

    License header added

--- src/main/java/nl/meine/ibb/stripes/ServerActionBean.java
+++ src/main/java/nl/meine/ibb/stripes/ServerActionBean.java
@@ -1,14 +1,22 @@
-
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2016 Meine Toonen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package nl.meine.ibb.stripes;
 
 import java.io.OutputStream;
-import java.math.BigInteger;
-import java.nio.ByteBuffer;
 import javax.servlet.http.HttpServletResponse;
 import net.sourceforge.stripes.action.ActionBean;
 import net.sourceforge.stripes.action.ActionBeanContext;
@@ -17,7 +25,6 @@ import net.sourceforge.stripes.action.Resolution;
 import net.sourceforge.stripes.action.StreamingResolution;
 import net.sourceforge.stripes.action.UrlBinding;
 import net.sourceforge.stripes.validation.Validate;
-import nl.meine.ibb.processor.ImageVectorizer;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
