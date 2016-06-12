b525c2505581bf7d21a057de7cd7043522709c9b
Meine Toonen <toonen.meine@gmail.com>
2016-06-12 12:23:36 +0200

    License header updated

--- src/main/java/nl/meine/ibb/processor/Processor.java
+++ src/main/java/nl/meine/ibb/processor/Processor.java
@@ -2,24 +2,33 @@
  * Copyright (C) 2016 Meine Toonen
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package nl.meine.ibb.processor;
+
+import java.io.File;
+import java.util.List;
+import nl.meine.ibb.stripes.Block;
 
 /**
  *
  * @author meine
  */
 public class Processor {
+    
+    
+    public List<Block> processImage(File img){
+        
+    }
     
 }
